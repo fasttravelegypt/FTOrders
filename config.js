94851bdaa15fdf4451fec5bd10271b114bcfe932
fasttravelegypt <fasttravelegypt@gmail.com>
2026-08-10 14:01:30 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbx5jBJMzP6TcRVZXPUY9UVhUdxrqigE6fU8Y_nv_FzTYmEPVTSunj6Z2miHIGCEi3bYkw/exec"
+};
+
+// Security Utility: Sanitize inputs to prevent XSS attacks
+function sanitizeHTML(str) {
+  const temp = document.createElement('div');
+  temp.textContent = str;
+  return temp.innerHTML;
+}
