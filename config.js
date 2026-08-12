e78f4680793b9d9f5b69a977f0db20a014794a03
fasttravelegypt <fasttravelegypt@gmail.com>
2026-08-12 14:30:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbx5jBJMzP6TcRVZXPUY9UVhUdxrqigE6fU8Y_nv_FzTYmEPVTSunj6Z2miHIGCEi3bYkw/exec"
+API_SECRET: "FtOrders_Sec9982Key!x" // Must match the EXPECTED_SECRET string set in your .gs script
 };
 
 // Security Utility: Sanitize inputs to prevent XSS attacks
