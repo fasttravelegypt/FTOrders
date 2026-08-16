f7ef19e506d71c29293c8393f1e2d570cd44d1d6
fasttravelegypt <fasttravelegypt@gmail.com>
2026-08-16 18:43:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycby0ZPSeUXhEhGrvvGxYfn3BvwjOLxpOleReKxApZ1IdbetjZwQk3zEOiiB1HtgLE_xGvg/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbylca1BO0aJ5nzX6rkGCFwMHJPs76BUe0jWDrq4fz6T_YcXEZ6oAnZ_iwtQ7-OdX8xF_A/exec"
 };
 
 // Security Utility: Sanitize inputs to prevent XSS attacks
